22bee23e50f539236aacd12af5c2de1626a643a8
HassanKhan123 <hk2602809@gmail.com>
2023-06-05 18:57:54 +0500

    added: fetch data from firebase

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -5,7 +5,6 @@ import 'package:http/http.dart' as http;
 
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
-import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -22,12 +21,12 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https('flutter-http-2d5df-default-rtdb.firebaseio.com',
           'shopping-list.json');
-      http.post(
+      await http.post(
         url,
         headers: {'Content-Type': 'application/json'},
         body: json.encode(
@@ -38,6 +37,9 @@ class _NewItemState extends State<NewItem> {
           },
         ),
       );
+
+      if (!context.mounted) return;
+      Navigator.of(context).pop();
     }
   }
 
